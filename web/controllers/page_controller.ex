c22bc5d8e4c4ab9780bbebd2974fdbff3bf24ba7
Connor Ford <connor.ford@ucdenver.edu>
2016-10-20 19:46:27 -0600

    Output connected users in template

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,13 +1,15 @@
+
 defmodule OwletChat.PageController do
   use OwletChat.Web, :controller
 
   @endpoint "https://owlet-users.firebaseio.com/users.json"
 
   def index(conn, params) do
-    %{body: body} = HTTPotion.get @endpoint
-    IO.puts(:Enum.map body, fn {k, v} -> v end)
+    user_ids = Map.keys(elem(Poison.Parser.parse((HTTPotion.get @endpoint).body), 1))
+
     conn
     |> assign(:room, params["room"])
+    |> assign(:users, user_ids)
     |> render("index.html")
   end
 end
